5aa447b0f98924d81e98ab3b85a3fa0fd0c38f7b
MKTkakumei <nizigenjyasinmkt@gmail.com>
2019-10-13 09:09:45 +0900

    no message

---
+++ Model/Assets/Kikukawa/Set.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Set : MonoBehaviour
+{
+    public GameObject obj;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            obj.SetActive(true);
+        }
+    }
+}
